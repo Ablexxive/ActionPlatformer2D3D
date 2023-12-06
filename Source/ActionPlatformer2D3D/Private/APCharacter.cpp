9c3d78de6d9cded802b2280d370a7219fd957381
Sahil Khanna <sahilsan@gmail.com>
2023-12-06 11:02:10 -0800

    Initial C++ character and RestarTesting Game Mode

--- Source/ActionPlatformer2D3D/Private/APCharacter.cpp
+++ Source/ActionPlatformer2D3D/Private/APCharacter.cpp
@@ -1,3 +1,9 @@
 
 #include "APCharacter.h"
 
+#include "IDetailTreeNode.h"
+
+AAPCharacter::AAPCharacter(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer)
+{
+}
--- Source/ActionPlatformer2D3D/Public/APCharacter.h
+++ Source/ActionPlatformer2D3D/Public/APCharacter.h
@@ -3,13 +3,19 @@
 #include "CoreMinimal.h"
 
 #include "PaperZDCharacter.h"
+#include "APCharacter.generated.h"
 
 //TODO Figure out log categories?? For next project maybe?
 
 UCLASS()
-class AP_API AAPCharacter : public ACharacter
+class  AAPCharacter : public APaperZDCharacter
 {
 	GENERATED_BODY()
 
 	AAPCharacter(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	// What will be in here:
+	// - Hitbox system??
+	// - Stun System
+	// - Damage system
 };
